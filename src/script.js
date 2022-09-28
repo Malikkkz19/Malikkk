a592e5355afabaea528161bd33803df297f5098a
Malikkk <mnizamidinov@yandex.ru>
2022-09-28 23:07:38 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+
+
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамиля?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+
+document.write(answers);
+
